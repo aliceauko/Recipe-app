c937905dc5d74d013b68861217b671fe46003603
aliceauko <alice.auko@student.moringaschool.com>
2022-07-28 11:54:18 +0300

    Add styling to the card component

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -5,7 +5,7 @@ function MealList() {
     <>
     <div className='card'>
         <img src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg" alt=""></img>
-        <h3>Spicy Arrabiata Penne</h3>
+        <h4>Spicy Arrabiata Penne</h4>
     </div>
     </>
   )
