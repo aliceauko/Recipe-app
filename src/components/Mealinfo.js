016275cea7ab4c5332f8fa04328a0cdce94d3762
aliceauko <alice.auko@student.moringaschool.com>
2022-07-28 15:21:15 +0300

    Display the fetched  data in the dom

--- src/components/Mealinfo.js
+++ src/components/Mealinfo.js
@@ -20,9 +20,24 @@ function Mealinfo() {
         <img src={items.strMealThumb} alt=""></img>
         <div className='inner-details'>
             <h1>{items.strMeal}</h1>
-            <h2>{items.strArea}Food</h2>
+            <h2>{items.strArea} Food</h2>
+            <h3>Category: {items.strCategory}</h3>
         </div>
 
+    </div>
+    <div className='res-details'>
+        <div className='ingredients'>
+            <h2>ingredients</h2>
+            <h4>{items.strIngredient1}:{items.strMeasure1}</h4>
+            <h4>{items.strIngredient2}:{items.strMeasure2}</h4>
+            <h4>{items.strIngredient3}:{items.strMeasure3}</h4>
+            <h4>{items.strIngredient4}:{items.strMeasure4}</h4>
+            <h4>{items.strIngredient5}:{items.strMeasure5}</h4>
+            <h4>{items.strIngredient6}:{items.strMeasure6}</h4>
+            <h4>{items.strIngredient7}:{items.strMeasure7}</h4>
+            <h4>{items.strIngredient8}:{items.strMeasure8}</h4>
+
+        </div>
     </div>
 
     </>)
