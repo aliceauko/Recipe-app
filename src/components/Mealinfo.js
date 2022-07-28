fd275348c956bd2da8b959bb276f9801648deb56
aliceauko <alice.auko@student.moringaschool.com>
2022-07-28 15:03:08 +0300

    Fetch meal info using the meals id

--- src/components/Mealinfo.js
+++ src/components/Mealinfo.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useParams} from 'react-router-dom';
+
 
 function Mealinfo() {
+    const[items, setItems]=useState();
+    const {MealId} = useParams();
+    if(MealId!=""){
+        fetch(`https:/www.themealdb.com/api/json/v1/1/lookup.php?i=${MealId}`)
+        .then(response=>response.json())
+        .then(data=>{
+            setItems(data.meals[0]);
+        })
+    }
   return (
   <>
-  <h4>Find your recipe information here</h4>
+  {
+    (!items)?"":(<>
+    <div className='details'>
+        <img src={items.strMealThumb} alt=""></img>
+        <div className='inner-details'>
+            <h1>{items.strMeal}</h1>
+            <h2>{items.strArea}Food</h2>
+        </div>
+
+    </div>
+
+    </>)
+  }
+ 
   </>
   )
 }
 
-export default Mealinfo
+export default Mealinfo;
