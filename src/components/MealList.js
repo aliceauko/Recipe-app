4d0322e11e7df5b0f4a0c049a81be8ed9f1b4e0b
aliceauko <alice.auko@student.moringaschool.com>
2022-07-28 11:46:07 +0300

    Create a component to hold the meal cards and import it to the meals component

---
+++ src/components/MealList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MealList() {
+  return (
+    <>
+    <div className='card'>
+        <img src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg" alt=""></img>
+        <h3>Spicy Arrabiata Penne</h3>
+    </div>
+    </>
+  )
+}
+
+export default MealList
---
+++ src/components/Meals.js
@@ -0,0 +1,30 @@
+import React from "react";
+import MealList from "./MealList";
+
+function Meals() {
+  return (
+    <>
+      <div className="meals">
+        <div className="header">
+          <h1>Search for food recipe</h1>
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            dolorem omnis quae consectetur saepe velit vitae, delectus corporis,
+            accusamus iste numquam quisquam quos? Quae suscipit dignissimos
+            veniam optio, aliquid ipsa?
+          </h4>
+        </div>
+
+        <div className="search">
+          <input type="text" className="searchbox"></input>
+        </div>
+        <div className="contain">
+          <MealList/>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Meals;
