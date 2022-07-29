5eee5c397db06f3b9f68a460ea35f395c17009a8
aliceauko <alice.auko@student.moringaschool.com>
2022-07-30 00:34:34 +0300

    Remove useState from the reviews file

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,54 +1,22 @@
-import React, { useState } from "react";
-import axios from "axios";
-
+import React from "react";
 function Reviews() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
-  console.log(name);
-  console.log(email);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    axios
-      .post(" http://localhost:5000/subscribe", {
-        name: name,
-        email: email,
-      })
-      .then(
-        (response) => {
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  };
-
-  return (
+return (
     <div className="form-div">
       <form className="review-form">
         <label>Name</label>
         <input
           placeholder="name"
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
         />
 
         <label>Email</label>
         <input
           placeholder="email"
           type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="submit"
           value="submit data"
-          className="btn"
-          onSubmit={handleSubmit}
         />
       </form>
     </div>
