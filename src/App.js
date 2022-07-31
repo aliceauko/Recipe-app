e6135e19265ce74ebec334fafedf1485d195afa9
aliceauko <alice.auko@student.moringaschool.com>
2022-07-31 23:41:07 +0300

    Fetch data from a fake json server and add post and delte features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Meals from "./components/Meals";
 import Mealinfo from "./components/Mealinfo";
-import Reviews from "./components/Subscribe";
+import Subscribe from "./components/Subscribe";
 import About from "./components/About";
 import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
@@ -14,7 +14,7 @@ function App() {
           <Route index element={<Meals />} />
           <Route path="/:MealId" element={<Mealinfo />} />
           <Route path="about" element={<About />} />
-          <Route path="reviews" element={<Reviews />} />
+          <Route path="reviews" element={<Subscribe />} />
         </Route>
       </Routes>
     </>
--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,24 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
 function Reviews() {
-return (
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        console.log("Getting response from ::::", response.data);
+        setData(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const postData = (e) => {
+    e.preventDefault();
+    Axios.post("https://jsonplaceholder.typicode.com/users", {
+      name,
+      email,
+    })
+      .then((response) => console.log("Posting data", response))
+      .catch((err) => console.log(err));
+  };
+
+  const postDelete = (id, e) => {
+    e.preventDefault();
+    Axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => {
+        console.log("Deleted !!!", response);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const arr = data.map((data, index) => {
+    return (
+      <tr>
+        <td style={{ border: "1px solid black" }}>{data.id}</td>
+        <td style={{ border: "1px solid black" }}> {data.name}</td>
+        <td style={{ border: "1px solid black" }}>{data.email}</td>
+        <td style={{ border: "1px solid black" }}>
+          <button onClick={(e) => postDelete(data.id, e)}>Delete</button>
+        </td>
+      </tr>
+    );
+  });
+  return (
     <div className="form-div">
       <form className="review-form">
         <label>Name</label>
         <input
           placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           type="text"
         />
 
         <label>Email</label>
         <input
           placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           type="text"
         />
-        <input
-          type="submit"
-          value="submit data"
-        />
+        <hr />
+        <button onclick={postData}>POST</button>
       </form>
+
+      <table style={{ border: "1px solid black" }}>
+        <tr>
+          <th style={{ border: "1px solid black" }}>ID</th>
+          <th style={{ border: "1px solid black" }}>Name</th>
+          <th style={{ border: "1px solid black" }}>Email</th>
+        </tr>
+
+        {arr}
+      </table>
     </div>
   );
 }
